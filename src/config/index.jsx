2f283f5b849e8d0315f8f41b160d779c878d23ed
szy1000 <1538078053@qq.com>
2018-04-10 23:29:55 +0800

    remove the whitespace

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -3,5 +3,5 @@ export const URL_HOME = ''
 export const URL_COMPANY = 'http://10.15.10.88:8080'
 
 
-export const IMAGE_PATH = 'https://raw.githubusercontent.com/szy1000/react/master/static/images/'
+export const IMAGE_PATH = 'https://raw.githubusercontent.com/szy1000/react/master/static/images/';
 
